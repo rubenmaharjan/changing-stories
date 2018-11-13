371ad983af3715aad8cb5ae7cc26a23c5e6d0054
Ruben Maharjan <ruben@debian.rubenmaharjan.com.np>
2018-11-13 14:34:22 +0545

    change added baseline

--- cs.py
+++ cs.py
@@ -15,10 +15,11 @@ class Student:
         self.grade = grade
         self.gender = gender
 
-nameCount =2
-ageCount =5
+nameCount = 2
+ageCount = 5
+baseCount = 6
 students = []
-i=0
+i = 0
 while banDevi['C'+str(nameCount)].value is not None:
     if banDevi['D'+str(ageCount)].value is None:
         gender = 'M'
@@ -26,10 +27,20 @@ while banDevi['C'+str(nameCount)].value is not None:
         gender = 'F'
     student = Student(banDevi['C'+str(nameCount)].value,banDevi['C'+str(ageCount)].value,banDevi['B'+str(ageCount)].value,gender)
     students.append(student)
-    nameCount +=8
-    ageCount +=8
-    print("hello",students[i].name,students[i].age,students[i].grade,students[i].gender)
-    i+=1
+    nameCount += 8
+    ageCount += 8
+    startChar = 'F'
+    for j in range(5):
+        #for litnum Nepali
+        print("timeline: ", banDevi[startChar+str(baseCount)].value)
+        print("value try", startChar, banDevi[startChar+str(baseCount)].value)
+        if banDevi[startChar+str(baseCount)].value is not None:
+            litNepali = j+1
+            print("lit marks", litNepali, j)
+            break
+        startChar = chr(ord(startChar)+1)
 
 
-
+    baseCount += 8
+    print("hello", students[i].name, students[i].age, students[i].grade, students[i].gender)
+    i += 1
